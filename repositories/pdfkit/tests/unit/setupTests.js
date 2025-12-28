1113d624591d9865d54f1b000933128f160e1978
sousaGab <gabrielamaralsousa@gmail.com>
2025-12-28 16:34:33 -0300

    fix: remove pdfkit

--- repositories/pdfkit/tests/unit/setupTests.js
+++
@@ -1,7 +0,0 @@
-import toContainChunk from './toContainChunk';
-import toContainText from './toContainText';
-import { toMatchImageSnapshot } from 'jest-image-snapshot';
-
-expect.extend(toContainChunk);
-expect.extend(toContainText);
-expect.extend({ toMatchImageSnapshot });
